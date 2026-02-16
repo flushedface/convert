aa2d6ce55189f53f53bda53bf528ec02610c04fa
p2r3 <p2r3@p2r3.com>
2026-02-16 18:08:26 +0200

    wrap format constructors in try/catch

--- src/handlers/index.ts
+++ src/handlers/index.ts
@@ -12,17 +12,18 @@ import qoiFuHandler from "./qoi-fu.ts";
 import sppdHandler from "./sppd.ts";
 import threejsHandler from "./threejs.ts";
 
-const handlers: FormatHandler[] = [
-  new canvasToBlobHandler(),
-  new meydaHandler(),
-  new FFmpegHandler(),
-  new pdftoimgHandler(),
-  new ImageMagickHandler(),
-  new renameHandler(),
-  new envelopeHandler(),
-  new svgForeignObjectHandler(),
-  new qoiFuHandler(),
-  new sppdHandler(),
-  new threejsHandler(),
-];
+const handlers: FormatHandler[] = [];
+try { handlers.push(new canvasToBlobHandler()) } catch (_) { };
+try { handlers.push(new canvasToBlobHandler()) } catch (_) { };
+try { handlers.push(new meydaHandler()) } catch (_) { };
+try { handlers.push(new FFmpegHandler()) } catch (_) { };
+try { handlers.push(new pdftoimgHandler()) } catch (_) { };
+try { handlers.push(new ImageMagickHandler()) } catch (_) { };
+try { handlers.push(new renameHandler()) } catch (_) { };
+try { handlers.push(new envelopeHandler()) } catch (_) { };
+try { handlers.push(new svgForeignObjectHandler()) } catch (_) { };
+try { handlers.push(new qoiFuHandler()) } catch (_) { };
+try { handlers.push(new sppdHandler()) } catch (_) { };
+try { handlers.push(new threejsHandler()) } catch (_) { };
+
 export default handlers;
